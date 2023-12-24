cd9e4204cc175ea6ea8d72513a5b07c504a4b385
abraaorochapb <abraaorochapb@gmail.com>
2023-12-24 14:34:28 -0300

    Adicionadas as principais rotas ao projeto, como são poucas eu decidi as colocar na file server mesmo

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,13 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response} from 'express';
+import { PrismaClient } from '@prisma/client';
+import { z } from 'zod';
 
-const vagasRoutes = require('./routes/vagasRoutes');
 const app = express();
 const port = 3000;
-
-app.use('/vagas', vagasRoutes);
-app.use('/postarvaga', vagasRoutes);
+const prisma = new PrismaClient();
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, World!');
+  res.send('Server is up and running!');
 });
 
 app.listen(port, () => {
@@ -17,8 +16,33 @@ app.listen(port, () => {
 
 app.use(express.json());
 
+app.get('/vagas', async (req: Request, res: Response) => {
+    const vagas = await prisma.vaga.findMany();
+    return res.json(vagas)
+})
 
+app.post('/postarvaga', async (req: Request, res: Response) => {
+    const createVagaSchema = z.object({
+        titulo: z.string(),
+        descricao: z.string(),
+        setor: z.string(),
+    })
+    const {titulo, descricao, setor} = createVagaSchema.parse(req.body)
+    await prisma.vaga.create({
+        data: {
+        titulo,
+        descricao, 
+        setor
+        }
+    })
+    return res.status(201).send()
+})
 
-
-
-
+app.get('/vagas/setor', async (req: Request, res: Response) => {
+  const vagas = await prisma.vaga.findMany({
+    where: {
+      setor: req.query.setor as string 
+    }
+  });
+  return res.json(vagas);
+});
