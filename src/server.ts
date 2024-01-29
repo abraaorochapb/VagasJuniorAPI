b8ac970016755506fb4206f262c10e55aa7069bb
abraaorochapb <abraaorochapb@gmail.com>
2024-01-28 22:43:41 -0300

    Adicionados parâmetros para requisições cross origin

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, { Request, Response} from 'express';
 import vagaRoutes from './Routes/vagaRoutes';
-import cors from 'cors';
 
 const app = express();
-app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
